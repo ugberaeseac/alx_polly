3e824ca622eebaf76a85a7fe10e4bf280e111551
ugbereaseac <ugberaeseac@gmail.com>
2025-09-05 12:02:49 -0400

    initialize the base NextJS project - alx_polly

---
+++ app/auth/page.tsx
@@ -0,0 +1,8 @@
+export default function AuthPage() {
+  return (
+    <div>
+      <h1>Authentication Page</h1>
+      {/* Add authentication components here */}
+    </div>
+  );
+}
---
+++ app/polls/page.tsx
@@ -0,0 +1,8 @@
+export default function PollsPage() {
+  return (
+    <div>
+      <h1>Polls Page</h1>
+      {/* Display polls here */}
+    </div>
+  );
+}
